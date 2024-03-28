e3e26b31efab85014a3fd382c940289082e7a81a
andreyviana <andreyviana125@gmail.com>
2024-03-27 22:16:17 -0300

    Script

---
+++ RenameAWAudioFiles/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace RenameAWAudioFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Program Files (x86)\Steam\steamapps\common\Call of Duty Advanced Warfare\portuguese";
+            string oldLang = "eng";
+            string newLang = "por";
+
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                if (file.Contains(oldLang))
+                    File.Move(file, file.Replace(oldLang, newLang));
+            }
+        }
+    }
+}
